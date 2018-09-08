c4fd09e1e90994017b0527d5ae453c83da130399
July-ss-2016 <july_ss_2016@outlook.com>
2018-09-08 08:52:23 +0800

    update

--- src/main/java/vip/ourcraft/programs/bossshoploganalyse/MainWindow.java
+++ src/main/java/vip/ourcraft/programs/bossshoploganalyse/MainWindow.java
@@ -24,8 +24,8 @@ public class MainWindow {
     private JPanel panel;
     private JButton runButton;
     private JTextField priceFileTextField;
-    private JTextField logFilePathTextField;
     private JButton cleanOldAnalysesButton;
+    private JTextArea logFilePathsTextArea;
 
     MainWindow() {
         runButton.addActionListener(e -> {
@@ -34,7 +34,7 @@ public class MainWindow {
                 return;
             }
 
-            if (logFilePathTextField.getText().equals("")) {
+            if (logFilePathsTextArea.getText().equals("")) {
                 sendWarningMsgBox("日志文件路径不能为空!");
                 return;
             }
@@ -47,170 +47,168 @@ public class MainWindow {
         });
         
         // 实现拖拽路径
-        registerTextFieldDropTarget(logFilePathTextField);
+        registerTextAreaDropTarget(logFilePathsTextArea);
         registerTextFieldDropTarget(priceFileTextField);
     }
 
     private void output() {
         File priceFile = new File(priceFileTextField.getText());
-        File logFile = new File(logFilePathTextField.getText());
 
-        if (!priceFile.exists()) {
-            sendWarningMsgBox("价格文件不存在!");
-            return;
-        }
+        for (String logFilePath : logFilePathsTextArea.getText().split("\n")) {
+            File logFile = new File(logFilePath);
 
-        if (!logFile.exists()) {
-            sendWarningMsgBox("日志文件不存在!");
-            return;
-        }
-
-        Workbook workbook = null;
-
-        try {
-            workbook = new XSSFWorkbook(new BufferedInputStream(new FileInputStream(priceFile), 2048)); // 不知道为什么不能用File作为参数, FileInputStream可行
-        } catch (IOException e) {
-            sendErrorMsgBox(e.getLocalizedMessage());
-            System.out.println(e.getMessage());
-        }
-
-        if (workbook == null || workbook.getNumberOfSheets() == 0) {
-            sendWarningMsgBox("没有找到数据表!");
-            return;
-        }
-
-        String logLines = Util.readFile(logFile);
-
-        if (logLines == null) {
-            sendWarningMsgBox("日志文件为空!");
-            return;
-        }
-
-        HashMap<String, BossShopLogAnalyse> bsLogAnalyses = new HashMap<>();
-
-        for (int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++) {
-            Sheet sheet = workbook.getSheetAt(sheetIndex);
-            String sheetName = sheet.getSheetName();
-            Row headerRow = sheet.getRow(0);
-
-            if (headerRow == null) {
-                continue;
+            if (!priceFile.exists()) {
+                sendWarningMsgBox("价格文件不存在!");
+                return;
             }
 
-            // 重置全局数据
-            BossShopLogAnalyse.resetGlobalData();
+            if (!logFile.exists()) {
+                sendWarningMsgBox("日志文件不存在!");
+                return;
+            }
 
-            // 逐行统计
-            for (String line : logLines.split(Util.LINE_SEPARATOR)) {
-                Gson gson = new Gson();
-                BossShopLog bossShopLog = gson.fromJson(line.substring(10), BossShopLog.class);
+            Workbook workbook = null;
 
-                if (!bossShopLog.getShopName().equalsIgnoreCase(sheetName)) {
+            try {
+                workbook = new XSSFWorkbook(new BufferedInputStream(new FileInputStream(priceFile), 2048)); // 不知道为什么不能用File作为参数, FileInputStream可行
+            } catch (IOException e) {
+                sendErrorMsgBox(e.getLocalizedMessage());
+                System.out.println(e.getMessage());
+            }
+
+            if (workbook == null || workbook.getNumberOfSheets() == 0) {
+                sendWarningMsgBox("没有找到数据表!");
+                return;
+            }
+
+            String logLines = Util.readFile(logFile);
+
+            if (logLines == null) {
+                sendWarningMsgBox("日志文件为空!");
+                return;
+            }
+
+            for (int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++) {
+                HashMap<String, BossShopLogAnalyse> bsLogAnalyses = new HashMap<>();
+                Sheet sheet = workbook.getSheetAt(sheetIndex);
+                String sheetName = sheet.getSheetName();
+                Row headerRow = sheet.getRow(0);
+
+                if (headerRow == null) {
                     continue;
                 }
 
-                String itemIndex = bossShopLog.getShopName() + ":" + bossShopLog.getItemName();
-                String player = bossShopLog.getPlayerName();
-                boolean isSellShop = bossShopLog.getReward().startsWith("[");
+                // 重置全局数据
+                BossShopLogAnalyse.resetGlobalData();
 
-                // 不存在统计类则创建
-                if (!bsLogAnalyses.containsKey(itemIndex)) {
-                    BossShopLogAnalyse temp = new BossShopLogAnalyse();
+                // 逐行统计
+                for (String line : logLines.split(Util.LINE_SEPARATOR)) {
+                    Gson gson = new Gson();
+                    BossShopLog bossShopLog = gson.fromJson(line.substring(10), BossShopLog.class);
 
-                    bsLogAnalyses.put(itemIndex, temp);
-                    temp.setItemIndex(itemIndex);
-                    temp.setItemInfo(isSellShop ? bossShopLog.getReward() : bossShopLog.getPrice());
-                    temp.setSourceFile(logFile);
+                    if (!bossShopLog.getShopName().equalsIgnoreCase(sheetName)) {
+                        continue;
+                    }
+
+                    String itemIndex = bossShopLog.getShopName() + ":" + bossShopLog.getItemName().replace("i", "");
+                    String player = bossShopLog.getPlayerName();
+                    boolean isSellShop = bossShopLog.getReward().startsWith("[");
+
+                    // 不存在统计类则创建
+                    if (!bsLogAnalyses.containsKey(itemIndex)) {
+                        BossShopLogAnalyse temp = new BossShopLogAnalyse();
+
+                        bsLogAnalyses.put(itemIndex, temp);
+                        temp.setItemIndex(itemIndex);
+                        temp.setItemInfo(isSellShop ? bossShopLog.getReward() : bossShopLog.getPrice());
+                        temp.setSourceFile(logFile);
+                    }
+
+                    // 得到统计结果，继续统计
+                    BossShopLogAnalyse logAnalyse = bsLogAnalyses.get(itemIndex);
+
+                    // 递增总价
+                    logAnalyse.addTotalTradedPrice(Double.parseDouble(isSellShop ? bossShopLog.getPrice() : bossShopLog.getReward()));
+                    // 递增交易次数
+                    logAnalyse.addTotalTradedCount(1);
+                    // 添加交易过的玩家id
+                    logAnalyse.putTradedPlayer(player);
                 }
 
-                // 得到统计结果，继续统计
-                BossShopLogAnalyse logAnalyse = bsLogAnalyses.get(itemIndex);
 
-                // 递增总价
-                logAnalyse.addTotalTradedPrice(Double.parseDouble(isSellShop ? bossShopLog.getPrice() : bossShopLog.getReward()));
-                // 递增交易次数
-                logAnalyse.addTotalTradedCount(1);
-                // 添加交易过的玩家id
-                logAnalyse.putTradedPlayer(player);
-            }
 
-            // 没统计数据直接跳过以下代码
-            if (bsLogAnalyses.size() == 0) {
-                continue;
-            }
-
-            int firstAnalyseHeader = headerRow.getLastCellNum();
-            final CellStyle CENTER_STYLE = workbook.createCellStyle();
-
-            CENTER_STYLE.setAlignment(XSSFCellStyle.ALIGN_CENTER);
-
-            // 设置统计表头
-            for (int i = 0; i < XLSX_HEADERS.length; i++) {
-                int index = firstAnalyseHeader + i;
-                Cell cell = headerRow.getCell(index);
-
-                if (cell == null) {
-                    cell = headerRow.createCell(index);
-                }
-
-                cell.setCellValue("BSLA-" + logFile.getName() + "-" + XLSX_HEADERS[i]);
-                cell.setCellStyle(CENTER_STYLE);
-            }
-
-            HashMap<String, Row> firstColumnRows = new HashMap<>();
-
-            // 把第一列对应的物品索引和表格索引存储起来
-            for (int i = 1; i < sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-
-                if (row == null) {
+                // 没统计数据直接跳过以下代码
+                if (bsLogAnalyses.size() == 0) {
                     continue;
                 }
 
-                Cell cell = row.getCell(0);
+                int firstAnalyseHeader = headerRow.getLastCellNum();
+                final CellStyle CENTER_STYLE = workbook.createCellStyle();
 
-                if (cell == null) {
-                    continue;
+                CENTER_STYLE.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+
+                // 设置统计表头
+                for (int i = 0; i < XLSX_HEADERS.length; i++) {
+                    int index = firstAnalyseHeader + i;
+                    Cell cell = headerRow.createCell(index);
+
+                    cell.setCellValue("BSLA-" + logFile.getName() + "-" + XLSX_HEADERS[i]);
+                    cell.setCellStyle(CENTER_STYLE);
                 }
 
-                String value = String.valueOf(sheet.getRow(i).getCell(0).getNumericCellValue());
-                int tmp = value.indexOf(".");
+                HashMap<String, Row> firstColumnRows = new HashMap<>();
 
-                firstColumnRows.put(sheet.getSheetName() + ":" + value.substring(0, tmp == -1 ? value.length() : tmp), sheet.getRow(i));
-            }
+                // 把第一列对应的物品索引和表格索引存储起来
+                for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                    Row row = sheet.getRow(i);
 
-            for (Map.Entry<String, BossShopLogAnalyse> entry : bsLogAnalyses.entrySet()) {
-                BossShopLogAnalyse analyse = entry.getValue();
-                // 在excel中物品索引没有i作为前缀
-                String itemIndex = entry.getKey().replace("i", "");
+                    if (row == null) {
+                        continue;
+                    }
 
+                    Cell cell = row.getCell(0);
 
-                if (firstColumnRows.containsKey(itemIndex)) {
-                    Row row = firstColumnRows.get(itemIndex);
+                    if (cell == null || cell.getCellType() != Cell.CELL_TYPE_NUMERIC) {
+                        continue;
+                    }
 
-                    // 填充数据
-                    row.createCell(firstAnalyseHeader).setCellValue(Util.formatDouble(analyse.getTotalTradedPrice()) + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedPrice() * 100) + "%)");
-                    row.createCell(firstAnalyseHeader + 1).setCellValue(analyse.getTotalTradedCount() + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedCount() * 100) + "%)");
-                    row.createCell(firstAnalyseHeader + 2).setCellValue(analyse.getTradedPlayerCount() + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedPlayerCount() * 100) + "%)");
+                    String value = String.valueOf(sheet.getRow(i).getCell(0).getNumericCellValue());
+                    int tmp = value.indexOf(".");
 
-                    // 设置居中
-                    row.getCell(firstAnalyseHeader).setCellStyle(CENTER_STYLE);
-                    row.getCell(firstAnalyseHeader + 1).setCellStyle(CENTER_STYLE);
-                    row.getCell(firstAnalyseHeader + 2).setCellStyle(CENTER_STYLE);
+                    firstColumnRows.put(sheet.getSheetName() + ":" + value.substring(0, tmp == -1 ? value.length() : tmp), sheet.getRow(i));
+                }
+
+                for (Map.Entry<String, BossShopLogAnalyse> entry : bsLogAnalyses.entrySet()) {
+                    BossShopLogAnalyse analyse = entry.getValue();
+                    String itemIndex = entry.getKey();
+
+                    if (firstColumnRows.containsKey(itemIndex)) {
+                        Row row = firstColumnRows.get(itemIndex);
+
+                        // 填充数据
+                        row.createCell(firstAnalyseHeader).setCellValue(Util.formatDouble(analyse.getTotalTradedPrice()) + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedPrice() * 100) + "%)");
+                        row.createCell(firstAnalyseHeader + 1).setCellValue(analyse.getTotalTradedCount() + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedCount() * 100) + "%)");
+                        row.createCell(firstAnalyseHeader + 2).setCellValue(analyse.getTradedPlayerCount() + "(" + Util.formatDouble(analyse.getPercentOfGlobalTradedPlayerCount() * 100) + "%)");
+
+                        // 设置居中
+                        row.getCell(firstAnalyseHeader).setCellStyle(CENTER_STYLE);
+                        row.getCell(firstAnalyseHeader + 1).setCellStyle(CENTER_STYLE);
+                        row.getCell(firstAnalyseHeader + 2).setCellStyle(CENTER_STYLE);
+                    }
+                }
+
+                for (int i = 0; i < headerRow.getPhysicalNumberOfCells(); i++) {
+                    sheet.autoSizeColumn(i);
                 }
             }
 
-            for (int i = 0; i < headerRow.getPhysicalNumberOfCells(); i++) {
-                sheet.autoSizeColumn(i);
+            try {
+                workbook.write(new FileOutputStream(priceFile));
+            } catch (IOException e) {
+                e.printStackTrace();
+                sendErrorMsgBox(e.getMessage());
+                return;
             }
-        }
-
-        try {
-            workbook.write(new FileOutputStream(priceFile));
-        } catch (IOException e) {
-            e.printStackTrace();
-            sendErrorMsgBox(e.getMessage());
-            return;
         }
 
         sendInfoMsgBox("更新文件完毕!");
@@ -224,16 +222,17 @@ public class MainWindow {
             return;
         }
 
-        Workbook priceWorkook = null;
+        Workbook priceWorkook;
 
         try {
             priceWorkook = new XSSFWorkbook(new BufferedInputStream(new FileInputStream(priceFile), 2048)); // 不知道为什么不能用File作为参数, FileInputStream可行
         } catch (IOException e) {
             sendErrorMsgBox(e.getLocalizedMessage());
             System.out.println(e.getMessage());
+            return;
         }
 
-        if (priceWorkook == null || priceWorkook.getNumberOfSheets() == 0) {
+        if (priceWorkook.getNumberOfSheets() == 0) {
             sendWarningMsgBox("没有找到数据表!");
             return;
         }
@@ -257,7 +256,7 @@ public class MainWindow {
             }
 
             // 删除指定列
-            for (int rowIndex = 0; rowIndex < sheet.getLastRowNum(); rowIndex++) {
+            for (int rowIndex = 0; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                 for (int column : analyseColumns) {
                     Row row = sheet.getRow(rowIndex);
 
@@ -299,6 +298,40 @@ public class MainWindow {
                         List<File> files = (List<File>) (event.getTransferable().getTransferData(DataFlavor.javaFileListFlavor));
 
                         textField.setText(files.get(0).getAbsolutePath());
+                        event.dropComplete(true);
+                    } catch (UnsupportedFlavorException | IOException e) {
+                        e.printStackTrace();
+                        sendErrorMsgBox(e.getLocalizedMessage());
+                    }
+
+                    return;
+                }
+
+                event.rejectDrop();
+            }
+        });
+    }
+
+    private void registerTextAreaDropTarget(JTextArea textArea) {
+        new DropTarget(textArea, DnDConstants.ACTION_COPY_OR_MOVE, new DropTargetAdapter() {
+            @Override
+            public void drop(DropTargetDropEvent event) {
+                if (event.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                    // 接收拖拽来的数据
+                    event.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+
+                    try {
+                        @SuppressWarnings("unchecked")
+                        List<File> files = (List<File>) (event.getTransferable().getTransferData(DataFlavor.javaFileListFlavor));
+
+                        for (File file : files) {
+                            if (!textArea.getText().equals("")) {
+                                textArea.append("\n");
+                            }
+
+                            textArea.append(file.getAbsolutePath());
+                        }
+
                         event.dropComplete(true);
                     } catch (UnsupportedFlavorException | IOException e) {
                         e.printStackTrace();
